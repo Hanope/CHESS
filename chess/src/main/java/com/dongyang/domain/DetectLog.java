31a1aaf4f543e46d6a6b387dac88f5474be4ff16
Hanope <peniewk@naver.com>
2016-11-20 20:31:52 +0900

    Final upload

---
+++ chess/src/main/java/com/dongyang/domain/DetectLog.java
@@ -0,0 +1,39 @@
+package com.dongyang.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * Created by HeemangHan on 2016. 10. 1..
+ */
+
+@Entity
+@Data
+public class DetectLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @ManyToOne
+    @JsonBackReference
+    private Device device;
+
+    @Column(nullable = false)
+    private boolean state;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp date;
+
+    public DetectLog() {
+
+    }
+
+    public DetectLog(Device device, boolean state) {
+        this.device = device;
+        this.state = state;
+    }
+}
